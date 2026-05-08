e6fadb42d0507ba5290a4d3b2b37e1c3e45e3c27
Maaz Mirza <maazmirza.co@gmail.com>
2026-05-08 21:12:04 +0000

    Learned how to use basic variables and data types in Python. Created a simple program that prints "Hello, World!" to the console. Also learned how to use comments to explain code and make it more readable.

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
+print("hello, world!") # first line ever written in python
+
+name = input("what's your name? ") # how to make a variable
+
+name = name.strip().capitalize() # how to remove whitespace from the beginning and end of a string and how to capitalize the first letter of a string
+
+print("hello,", name + ",", "You are awesome!") #how to print multiple things at once, and how to concatenate strings with variables
+
+print('hello,', end=' ', sep='') # how to change the end of a print statement, and how to change the separator between items in a print statement
+
+print(name + "!") # how to use the end and sep parameters in a print statement to create a custom output format
+
+print("have a \"beautiful\" Day!") # how to use escape characters to include special characters in a string, such as quotes or newlines
+
+print(f"hello, {name}!") # how to use f-strings to include variables in a string without concatenation
+
+
 
