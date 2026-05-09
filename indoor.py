26a9557a2bb6d2b2fb928b6817b6e6db64db3441
Maaz Mirza <maazmirza.co@gmail.com>
2026-05-09 15:56:54 +0000

    Indoor.py exercise CS50P

---
+++ indoor.py
@@ -0,0 +1,5 @@
+txt = input("Write something : ").strip()
+
+print("here is what you wrote in uppercase : ", txt.upper())
+print("here is what you wrote in lowercase : ", txt.lower())
+print("here is what you wrote in title case : ", txt.title())
