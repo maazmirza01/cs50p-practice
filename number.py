85cab8d50ef64f98938dfeafd2f77667decdd3cf
Maaz Mirza <maazmirza.co@gmail.com>
2026-05-12 18:28:33 +0000

    Lecture 3 Exceptions CS50P.

---
+++ number.py
@@ -0,0 +1,37 @@
+while True:
+    try:
+        x = int(input("Whats x ? "))    # keep the code in the try op as short/minimal as possible./ we could put break under try to too
+    except ValueError:
+        print("x is not an integer! ERROR!")
+    else:
+        break
+
+
+print(f"x is {x}")
+
+# alt way to do this:
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+             x = int(input("Whats x ? ")) 
+             break           # keep the code in the try statement as short/minimal as possible./ we could put break under try to too
+        except ValueError:
+             print("x is not an integer! ERROR!")
+    return x
+
+main()
+
+
+
+
+
+
+
+
+
+
